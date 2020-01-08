24cbd210214babde4e60b4d5fe4fe98c108db229
j070249 <j070249@gmail.com>
2020-01-08 10:20:27 +0800

    v1.0.0

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # GtechBle-iOS
  
 需要使用cocoapod加入 pod 'RxBluetoothKit'
+
+可參考ViewController.swift範例程式
---
+++ WhizConnect-SDK/whizconnect_sdk.framework/Headers/whizconnect_sdk.h
@@ -0,0 +1,19 @@
+//
+//  whizconnect_sdk.h
+//  whizconnect-sdk
+//
+//  Created by Jimmy Tai on 2020/1/3.
+//  Copyright © 2020 SEDA G-Tech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for whizconnect_sdk.
+FOUNDATION_EXPORT double whizconnect_sdkVersionNumber;
+
+//! Project version string for whizconnect_sdk.
+FOUNDATION_EXPORT const unsigned char whizconnect_sdkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <whizconnect_sdk/PublicHeader.h>
+
+
